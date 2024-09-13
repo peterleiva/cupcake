5ffa6b14588c1ee1f73e39ad7b15ca1c1d8723e3
Peter <github@peter.dev.br>
2024-09-12 22:36:28 -0300

    chore: add react native paper

---
+++ apps/mobile/src/app/products.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Products page</p>
+      <Link></Link>
+    </div>
+  );
+}
